85b21887b7fb937f46849be0180ab0fa9329d235
PratikG345 <mrpratikonline@gmail.com>
2025-11-02 15:05:17 +0530

    static classes

--- Example.java
+++ Example.java
@@ -1,5 +1,26 @@
+class Pratik{
+    static void fun1(){
+        System.out.println("Class A Called");
+    }
+    static void fun2(){
+        System.out.println("Class B called");
+    }
+}
+
 class Example{
+
+    static void fun1(){
+        System.out.println("A Called");
+    }
+    static void fun2(){
+        System.out.println("B called");
+    }
     public static void main(String[] args){
         System.out.println("Hello Pratik");
+        fun1();
+        fun2();
+        Pratik.fun1();
+        Pratik.fun2();
+
     }
 }
