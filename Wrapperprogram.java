725ecdaf33af0b555c9372d228c657d9cc0747bb
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-11-04 14:56:35 +0530

    wrapper code

--- Wrapperprogram.java
+++ Wrapperprogram.java
@@ -1,3 +1,12 @@
 public class Wrapperprogram {
-    
+    public static void main(String[] args) {
+        int x = Integer.parseInt("123");
+        Integer x1 = Integer.valueOf("101100",2);
+        Double x2 = Double.valueOf("44");
+        int y = x1.intValue();
+        double z = x2.intValue();
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
 }
